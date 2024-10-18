00530e7dfe8ff6c88868e77b1fa4ae8c5d292057
ham <ham.experiment1@gmail.com>
2024-10-18 09:31:50 +0200

    onSelectImage function

--- src/main/java/com/example/apptestingjavafx/HelloController.java
+++ src/main/java/com/example/apptestingjavafx/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.apptestingjavafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    public void initialize() {
+        System.out.println();
+    }
+
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSelectImage() {
+        System.out.println(45);
     }
 }
