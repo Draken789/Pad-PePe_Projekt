51d1dc4c675049ade267f1091cee97f2ae5fdd70
ham <ham.experiment1@gmail.com>
2024-10-18 10:09:04 +0200

    OpenFileVia explorer WIP

--- src/main/java/com/example/apptestingjavafx/HelloController.java
+++ src/main/java/com/example/apptestingjavafx/HelloController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -15,5 +19,22 @@ public class HelloController {
     @FXML
     protected void onSelectImage() {
         System.out.println(45);
+    }
+
+    public static boolean OpenFileViaExplorer() {
+        try {
+            JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Image files", "png", "jpg");
+            fileChooser.setFileFilter(filter);
+            fileChooser.setCurrentDirectory(new File("."));
+            int result = fileChooser.showOpenDialog(null);
+            System.out.println("Result: " + result);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+
+        return false;
     }
 }
